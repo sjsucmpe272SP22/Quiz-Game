2404d0c714a5e1a2f88e21734f818dd36ef9f1ea
Sai Manasa Yadlapalli <manasa.yadlapalli5@gmail.com>
2022-04-06 15:12:27 -0700

    Added username and highscore in localstorage

--- assets/js/user.js
+++ assets/js/user.js
@@ -1,1 +1,28 @@
 const username = document.querySelector("#userName");
+const userHighScore = document.querySelector("#highScore");
+const finalLabel = document.querySelector("#finalModalLabel");
+
+const topScore = localStorage.getItem("topScore");
+userHighScore.innerText = topScore;
+
+
+let user = localStorage.getItem("user");
+let highScore = 0;
+const checkUserData = () => {
+    if (user === null || user === "") {
+        localStorage.setItem("highScore", 0);
+        $("#playerModal").modal("show");
+        $("#playerModal").ready(function () {
+            $('#user').focus();
+        });
+
+    } else {
+        user = localStorage.getItem("user");
+        highScore = localStorage.getItem("highScore");
+        if (highScore === null) {
+            highScore = 0;
+        }
+
+        displayUsernameInfo();
+    }
+};
