ee5b9c509ee01c2151636b799e5dcc42ee6be105
Sai Manasa Yadlapalli <manasa.yadlapalli5@gmail.com>
2022-04-06 15:15:37 -0700

    Created User Highscore function

--- assets/js/user.js
+++ assets/js/user.js
@@ -26,3 +26,30 @@ const checkUserData = () => {
         displayUsernameInfo();
     }
 };
+
+const setHighScore = () => {
+    let gameScore = parseInt(currentScore.innerText);
+    if (gameScore > userHighScore.innerText && gameScore !== undefined) {
+        finalLabel.innerText = "CONGRATULATIONS!!!";
+        finalScore.innerText = `NEW HIGH SCORE!!! You scored ${gameScore} points`;
+        userHighScore.innerText = gameScore;
+        localStorage.setItem("highScore", gameScore);
+        if (document.querySelector(".fa-volume-up ")) {
+            fanfare.play();
+
+        }
+
+    } else if (gameScore <= userHighScore.innerText && gameScore !== undefined) {
+        finalLabel.innerText = "RESULT";
+        finalScore.innerText = `Your scored ${gameScore} points`;
+        if (document.querySelector(".fa-volume-up ")) {
+            finalSound.play();
+        }
+    } else {
+        finalLabel.innerText = "UNFORTUNATELY";
+        finalScore.innerText = "No point this time.Try Again!";
+        if (document.querySelector(".fa-volume-up ")) {
+            noPoints.play();
+        }
+    }
+};
