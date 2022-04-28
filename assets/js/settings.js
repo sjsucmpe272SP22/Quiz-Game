1178818e9f5f84cfbf1e5579eb66fc11b374b4b2
Mounica Reddy Kandi <99624135+MounicaKandi@users.noreply.github.com>
2022-04-27 17:32:07 -0700

    Settings page update

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -1,1 +1,39 @@
-// Game settings
+// *****************************************Game Settings*****************************************
+
+// selecting and getting diffculty level and categories
+const difficulty = document.querySelector("#difficulty");// also without Arrayfrom and then p2
+const categories = document.querySelector("#categories");
+const gameProgress = document.querySelector('#wrapper');
+let difficultyLevel;
+let categoryId;
+// toggle sound icon on
+const iconOn = document.querySelector("#off");
+
+switcher = () => {
+    iconOn.classList.toggle("fa-volume-up");
+};
+
+iconOn.addEventListener("click", switcher);
+// Click Sound when volume on
+const click = document.querySelector("#btnClick");
+const allButtons = document.querySelectorAll(".btn");
+
+clickBtn = () => {
+    if (document.querySelector(".fa-volume-up ")) {
+        click.play();
+    }
+};
+Array.from(allButtons).forEach((a) => {
+    a.addEventListener("click", clickBtn);
+});
+
+
+    difficulty.addEventListener("change", (e) => {
+        difficultyLevel = e.target.value;
+    });
+
+
+
+    categories.addEventListener("change", (e) => {
+        categoryId = e.target.value;
+    });
